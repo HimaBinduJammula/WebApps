00c6e55e63e15587537fd3908d53a9749374732d
harshaalla <s528101@mail.nwmissouri.edu>
2016-11-02 21:58:15 -0500

    Committed by 5-12

--- controllers/aggregateMaterials.js
+++ controllers/aggregateMaterials.js
@@ -3,10 +3,19 @@ var api = express.Router();
 
 // see app.js for the root request this controller handles
 
-// GET to this controller root URI
-api.get("/", function (req, res) {
-  return res.render('aggregate/index.ejs');
+api.get('/findall', function(req, res){
+res.setHeader('Content-Type', 'application/json');
+var data = req.app.locals.aggregateMaterials.query;
+res.send(JSON.stringify(data));
 });
 
+// GET to this controller root URI
+api.get("/", function (req, res) {
+return res.render('aggregate/index.ejs');
+});
 
 module.exports = api;
+
+//This model is managed by Team 5-12
+//Harsha Vardhan Reddy Alla
+//Saiteja Guduri
