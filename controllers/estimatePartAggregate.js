31b9f7d32ca96dd6407fe1ac8012d5020f49f13f
SriSaiRavali <ravalichinthareddy111@gmail.com>
2016-11-13 23:28:59 -0600

    initial commit

--- controllers/estimatePartAggregate.js
+++ controllers/estimatePartAggregate.js
@@ -30,4 +30,60 @@ api.get('/findall', function(req, res){
 module.exports = api;  // at the very end
 
 
+// GET create
+api.get("/create", function(req, res) {
+    console.log('Handling GET /create' + req);
+    res.render("aggregate_cost/create.ejs",
+        { title: " Aggregate", layout: "layout.ejs" });
+});
+
+
+// DELETE
+api.get('/delete/:id', function(req, res) {
+    console.log("Handling GET /delete/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.estimatePartAggregates.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('aggregate_cost/delete.ejs',
+        {
+            title: "Footage",
+            layout: "layout.ejs",
+            estimatePartAggregate: item
+        });
+});
+
+// GET /details/:id
+api.get('/details/:id', function(req, res) {
+    console.log("Handling GET /details/:id " + req);
+    var id = parseInt(req.params.sqft);
+    var data = req.app.locals.estimatePartAggregates.query;
+    var item = find(data, { 'sqft': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('aggregate_cost/details.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            estimatePartAggregate: item
+        });
+});
+
+// GET one
+api.get('/edit/:id', function(req, res) {
+    console.log("Handling GET /edit/:id " + req);
+    var id = parseInt(req.params.sqft);
+    var data = req.app.locals.estimatePartAggregates.query;
+    var item = find(data, { 'sqft': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('aggregate_cost/edit.ejs',
+        {
+            title: "WP Primers",
+            layout: "layout.ejs",
+            estimatePartAggregate: item
+        });
+});
+
 
