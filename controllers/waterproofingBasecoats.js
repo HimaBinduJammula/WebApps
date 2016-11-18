38070afa321bc1bb316f675846452eccd59eb88c
Sruthi <s528163@mail.nwmissouri.edu>
2016-11-17 23:21:58 -0600

    5-14 Commit

--- controllers/waterproofingBasecoats.js
+++ controllers/waterproofingBasecoats.js
@@ -18,7 +18,17 @@ api.get('/findall', function(req, res){
     var data = req.app.locals.waterproofingBasecoats.query;
     res.send(JSON.stringify(data));
 });
-
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', function(req, res, next) {
+    console.log("Handling DELETE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling REMOVING ID=" + id);
+    var data = req.app.locals.waterproofingPrimers.query;
+    var item = remove(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("Deleted item " + JSON.stringify(item));
+    return res.redirect('/waterproofingPrimer');
+});
 
 
 
