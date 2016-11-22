df401d47b9a89405569326804135c34b590071dd
Sai Prakash <s528151@mail.nwmissouri.edu>
2016-11-21 23:38:26 -0600

    signed in all folders

--- controllers/roofingEstimates.js
+++ controllers/roofingEstimates.js
@@ -13,12 +13,17 @@ const notfoundstring = 'No such roofing Estimate';
 // HTML5 forms can only have GET and POST methods (use POST for DELETE)
 
 
-// HANDLE JSON REQUESTS --------------------------------------------
-api.get('/findall', function(req, res){
-    res.setHeader('Content-Type', 'application/json');
-    var data = req.app.locals.roofingEstimates.query;
-    res.send(JSON.stringify(data));
+api.get("/", function (request, response) {
+ response.render("roofing/roofing.ejs");
 });
+
+// Handle Json Requests
+api.get('/findall', function(req, res){
+  res.setHeader('Content-type', 'application/json');
+  var data = req.app.locals.roofingEstimates.query;
+  res.send(JSON.stringify(data));
+});
+
 api.get('/findone/:id', function(req, res){
      res.setHeader('Content-Type', 'application/json');
     var id = parseInt(req.params.id);
@@ -28,150 +33,116 @@ api.get('/findone/:id', function(req, res){
     res.send(JSON.stringify(item));
 });
 
-
-// roofing routing relative to views folder declared in app.js
-
-// GET roofing default page
-api.get("/", function (request, response) {
-  response.render("roofing/roofing.ejs");
+// GET roofing-new
+api.get("/create", function (request, response) {
+  response.render("roofing/create.ejs");
 });
 
-api.get("/details", function (request, response) {
-  response.render("./roofing/details.ejs");
+// POST new
+api.post('/save', function(req, res) {
+     console.log("Handling POST " + req);
+     var data = req.app.locals.roofingEstimates.query;
+     var item = new Model;
+     console.log("NEW ID " + req.body._id);
+     item._id = parseInt(req.body._id);
+    // item.estimatePartAbout.client = req.body.Client;
+    // item.estimatePartAbout.created = req.body.Created;
+    // item.estimatePartAbout.address = req.body.Address;
+    // item.estimatePartAbout.city = req.body.City;
+    // item.estimatePartAbout.state = req.body.State;
+    // item.estimatePartAbout.zipcode = req.body.zipcode;
+    // item.estimateSquareFootage.sqft = req.body.Squre_Footage;
+    // item.costPerSquareFoot = req.body.CostPerSquareFoot;
+    // item.bidPerSquareFoot = req.body.BidPerSquareFoot;
+     return res.redirect('/roofingEstimate');
 });
 
-api.get("/delete", function (request, response) {
-  response.render("./roofing/delete.ejs");
+// GET /details/:id
+api.get('/details/:id', function(req, res) {
+    console.log("Handling GET /details/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.roofingEstimates.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render("roofing/details.ejs",
+        {
+            title: "roofing Estimate",
+            layout: "layout.ejs",
+            roofingEstimate: item
+        });
 });
-api.get("/edit", function (request, response) {
-  response.render("./roofing/edit.ejs");
+
+// GET one Edit
+api.get('/edit/:id', function(req, res) {
+    console.log("Handling GET /edit/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.roofingEstimates.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render('roofing/edit.ejs',
+        {
+            title: "roofing Estimate",
+            layout: "layout.ejs",
+            roofingEstimate: item
+        });
 });
 
+//Edit roofing Estimate SAVE
+// POST update
+api.post('/save/:id', function(req, res) {
+    console.log("Handling SAVE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling SAVING ID=" + id);
+    var data = req.app.locals.roofingEstimates.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("ORIGINAL VALUES " + JSON.stringify(item));
+    console.log("UPDATED VALUES: " + JSON.stringify(req.body));
+    item.estimatePartAbout.client = req.body.Client;
+    item.estimatePartAbout.created = req.body.Created;
+    item.estimatePartAbout.address = req.body.Address;
+    item.estimatePartAbout.city = req.body.City;
+    item.estimatePartAbout.state = req.body.State;
+    item.estimatePartAbout.zipcode = req.body.zipcode;
+    item.estimateSquareFootage.sqft = req.body.Squre_Footage;
+    item.costPerSquareFoot = req.body.CostPerSquareFoot;
+    item.bidPerSquareFoot = req.body.BidPerSquareFoot;
+    
+    console.log("SAVING UPDATED ITEM " + JSON.stringify(item));
+    return res.redirect('/roofingEstimate');
+});
 
-// GET create
-// api.get("/create", function (request, response) {
-//   response.render("roofing/create.ejs");
-// });
+// GET on Delete
+api.get('/delete/:id', function(req, res) {
+    console.log("Handling GET /delete/:id " + req);
+    var id = parseInt(req.params.id);
+    var data = req.app.locals.roofingEstimates.query;
+    var item = find(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("RETURNING VIEW FOR" + JSON.stringify(item));
+    return res.render("roofing/delete.ejs",
+        {
+            title: "roofing Estimate",
+            layout: "layout.ejs",
+            roofingEstimate: item
+        });
+});
 
-// module.exports = api;
+// DELETE id (uses HTML5 form method POST)
+api.post('/delete/:id', function(req, res, next) {
+    console.log("Handling DELETE request" + req);
+    var id = parseInt(req.params.id);
+    console.log("Handling REMOVING ID=" + id);
+    var data = req.app.locals.roofingEstimates.query;
+    var item = remove(data, { '_id': id });
+    if (!item) { return res.end(notfoundstring); }
+    console.log("Deleted item " + JSON.stringify(item));
+    return res.redirect('/roofingEstimate');
+});
+
+module.exports = api;
 //This model is managed by Team 5-02 
 // Mekonda, Sai Prakash 
 // Kovuri, Revanth 
-//--------------------------------------------
-
-
-
-
-// HANDLE VIEW DISPLAY REQUESTS --------------------------------------------
-
-// GET all
-api.get('/', function(req, res) {
-    console.log("Handling GET " + req);
-    return res.render('roofing/index.ejs',
-        { title: "R Estimates", layout: "layout.ejs" });
-});
-
-// GET create
-api.get("/create", function(req, res) {
-    console.log('Handling GET /create' + req);
-    res.render("roofing/create.ejs",
-        { title: "R Estimates", layout: "layout.ejs" });
-});
-
-// GET /delete/:id
-// api.get('/delete/:id', function(req, res) {
-//     console.log("Handling GET /delete/:id " + req);
-//     var id = parseInt(req.params.id);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = find(data, { '_id': id });
-//     if (!item) { return res.end(notfoundstring); }
-//     console.log("RETURNING VIEW FOR" + JSON.stringify(item));
-//     return res.render('roofing/delete.ejs',
-//         {
-//             title: "R Estimates",
-//             layout: "layout.ejs",
-//             roofingEstimate: item
-//         });
-// });
-
-// // GET /details/:id
-// api.get('/details/:id', function(req, res) {
-//     console.log("Handling GET /details/:id " + req);
-//     var id = parseInt(req.params.id);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = find(data, { '_id': id });
-//     if (!item) { return res.end(notfoundstring); }
-//     console.log("RETURNING VIEW FOR" + JSON.stringify(item));
-//     return res.render('roofing/details.ejs',
-//         {
-//             title: "R Estimates",
-//             layout: "layout.ejs",
-//             roofingEstimate: item
-//         });
-// });
-
-// // GET one
-// api.get('/edit/:id', function(req, res) {
-//     console.log("Handling GET /edit/:id " + req);
-//     var id = parseInt(req.params.id);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = find(data, { '_id': id });
-//     if (!item) { return res.end(notfoundstring); }
-//     console.log("RETURNING VIEW FOR" + JSON.stringify(item));
-//     return res.render('roofing/edit.ejs',
-//         {
-//             title: "R Estimates",
-//             layout: "layout.ejs",
-//             roofingEstimate: item
-//         });
-// });
-
-// // HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
-
-// // POST new
-// api.post('/save', function(req, res) {
-//     console.log("Handling POST " + req);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = new Model;
-//     console.log("NEW ID " + req.body._id);
-//     item._id = parseInt(req.body._id);
-//     item.name = req.body.name;
-//     item.unit = req.body.unit;
-//     item.price = req.body.price;
-//     item.displayorder = parseInt(req.body.displayorder);
-//     data.push(item);
-//     console.log("SAVING NEW ITEM " + JSON.stringify(item));
-//     return res.redirect('/roofingEstimate');
-// });
-
-// // POST update
-// api.post('/save/:id', function(req, res) {
-//     console.log("Handling SAVE request" + req);
-//     var id = parseInt(req.params.id);
-//     console.log("Handling SAVING ID=" + id);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = find(data, { '_id': id });
-//     if (!item) { return res.end(notfoundstring); }
-//     console.log("ORIGINAL VALUES " + JSON.stringify(item));
-//     console.log("UPDATED VALUES: " + JSON.stringify(req.body));
-//     item.name = req.body.name;
-//     item.unit = req.body.unit;
-//     item.price = req.body.price;
-//     item.displayorder = req.body.displayorder;
-//     console.log("SAVING UPDATED ITEM " + JSON.stringify(item));
-//     return res.redirect('/roofingEstimate');
-// });
-
-// // DELETE id (uses HTML5 form method POST)
-// api.post('/delete/:id', function(req, res, next) {
-//     console.log("Handling DELETE request" + req);
-//     var id = parseInt(req.params.id);
-//     console.log("Handling REMOVING ID=" + id);
-//     var data = req.app.locals.roofingEstimates.query;
-//     var item = remove(data, { '_id': id });
-//     if (!item) { return res.end(notfoundstring); }
-//     console.log("Deleted item " + JSON.stringify(item));
-//     return res.redirect('/roofingEstimate');
-// });
-
-module.exports = api;
