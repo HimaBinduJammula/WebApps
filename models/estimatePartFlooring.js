d5d7335128e8b9b4ac2ab4c6ce904ac98943c429
Saravana <S528161@mail.nwmssouri.edu>
2016-11-21 21:06:51 -0600

    M13

--- models/estimatePartFlooring.js
+++ models/estimatePartFlooring.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
   , FlooringCoating = require('./flooringCoating.js')
 
 var EstimatePartFlooringSchema = new Schema({
+  _id: { type: Number, required: true },
   floorSystemType: { type: String, required: true, default: 'Epoxy', enum: ['Epoxy', 'Decorative Expoxy', 'Urethane'] },
   usesUrethane: { type: Boolean, required: true, default: false },
   urethaneProductSelection: [{ type: Schema.Types.ObjectId, ref: FlooringCoating, required: false }],
@@ -16,40 +17,7 @@ var EstimatePartFlooringSchema = new Schema({
 var estimatePartFlooring = mongoose.model('EstimatePartFlooring', EstimatePartFlooringSchema)
 module.exports = estimatePartFlooring
 
-// <<<<<<< HEAD
-
-//<<<<<<< HEAD
 // This data is managed by Team 5-05
 // Lakshmi Supriya Nakerikanti
 // Saravana Kumar Palaniyappan
 
-
-// This data is managed by Team 5-05
-// Lakshmi Supriya Nakerikanti
-// Saravana Kumar Palaniyappan
-
-// This model is managed by Team 5-5
-// Saravana Kumar Palaniyappan
-// Lakshmi Supriya Nakerikanti
-
-// This model is managed by Team 5-5
-// Saravana Kumar Palaniyappan
-// Lakshmi Supriya Nakerikanti// >>>>>>> d8c94698ebb1872afe5b5e6c4859b8746cfff6a2
-
-// This data is managed by Team 5-05
-// Lakshmi Supriya Nakerikanti
-
-//>>>>>>> d8c94698ebb1872afe5b5e6c4859b8746cfff6a2
-
-// Saravana Kumar Palaniyappan
-
-// This model is managed by Team 5-5
-// Saravana Kumar Palaniyappan
-// Lakshmi Supriya Nakerikanti
-
-// =======
-
-// This data is managed by Team 5-05
-// Lakshmi Supriya Nakerikanti
-// Saravana Kumar Palaniyappan
-// >>>>>>> 5eb376028265ffc69ba0a3ae967b80e036bd1bae
